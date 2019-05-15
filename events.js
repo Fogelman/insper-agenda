95b6f98b932a9cdaf92b426706c6a8a483bb4828
fogelman <ddfogelman@gmail.com>
2019-05-15 12:31:58 -0300

    Adding titleCase function

--- events.js
+++ events.js
@@ -7,64 +7,18 @@ BASE64 = str => {
   return base64data;
 };
 
-// titleCase = str => {
-//   console.log(str);
-//   if (str.indexOf(' ') > -1) {
-//     return str
-//       .toLowerCase()
-//       .split(' ')
-//       .map(word => {
-//         return word.replace(word[0], word[0].toUpperCase());
-//       })
-//       .join(' ');
-//   } else {
-//     return str;
-//   }
-// };
-
-/*
- * https://github.com/gouch/to-title-case/blob/master/to-title-case.js
- */
-String.prototype.toTitleCase = function() {
-  'use strict';
-  var smallWords = /^(a|an|and|as|at|but|by|en|for|if|in|nor|of|on|or|per|the|to|v.?|vs.?|via)$/i;
-  var alphanumericPattern = /([A-Za-z0-9\u00C0-\u00FF])/;
-  var wordSeparators = /([ :–—-])/;
-
-  return this.split(wordSeparators)
-    .map(function(current, index, array) {
-      if (
-        /* Check for small words */
-        current.search(smallWords) > -1 &&
-        /* Skip first and last word */
-        index !== 0 &&
-        index !== array.length - 1 &&
-        /* Ignore title end and subtitle start */
-        array[index - 3] !== ':' &&
-        array[index + 1] !== ':' &&
-        /* Ignore small words that start a hyphenated phrase */
-        (array[index + 1] !== '-' ||
-          (array[index - 1] === '-' && array[index + 1] === '-'))
-      ) {
-        return current.toLowerCase();
-      }
-
-      /* Ignore intentional capitalization */
-      if (current.substr(1).search(/[A-Z]|\../) > -1) {
-        return current;
-      }
-
-      /* Ignore URLs */
-      if (array[index + 1] === ':' && array[index + 2] !== '') {
-        return current;
-      }
-
-      /* Capitalize the first letter */
-      return current.replace(alphanumericPattern, function(match) {
-        return match.toUpperCase();
-      });
-    })
-    .join('');
+titleCase = str => {
+  if (str.indexOf(' ') > -1) {
+    return str
+      .toLowerCase()
+      .split(' ')
+      .map(word => {
+        return word.replace(word[0], word[0].toUpperCase());
+      })
+      .join(' ');
+  } else {
+    return str;
+  }
 };
 
 class Events {
@@ -130,7 +84,7 @@ class Events {
       msg += '</b> \n';
 
       msg += date[1] + ' - ' + date[3] + '\n';
-      msg += '<b>' + element.title.toLowerCase().toTitleCase() + '</b> \n';
+      msg += '<b>' + titleCase(element.title) + '</b> \n';
       msg += '<i>' + element.room + '</i> \n';
       msg += '\n';
     });
